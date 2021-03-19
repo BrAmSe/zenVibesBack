4d6f086cbcc5a20d43ea90459486205212e6731c
bram <brais.amo@gmail.com>
2021-03-19 13:37:36 +0100

    Flyway migration system added

--- src/main/java/com/bramse/zenvibes/config/DataSourceConfig.java
+++ src/main/java/com/bramse/zenvibes/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.bramse.zenvibes.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
 import java.util.Map;
@@ -13,29 +14,50 @@ public class DataSourceConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/zen_vibes";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
+    private static final Map<String, String> env = System.getenv();
 
     @Bean
     public DataSource getDataSource() {
+        // Definimos los parametros de configuración
+        String url = getDataSourceUrl();
+        String user = getDataSourceUsername();
+        String password = getDatasourcePassword();
+
+        // Configuramos el Datasource
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        // Obtenemos los parametros de configuración del sistema
-        Map<String, String> env = System.getenv();
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(user);
+        dataSourceBuilder.password(password);
+
+        // Configuramos Flyway y lanzamos las migraciones
+        Flyway flyway = Flyway.configure().dataSource(url, user, password).load();
+        flyway.migrate();
+
+        // Devolvemos en datasource
+        return dataSourceBuilder.build();
+    }
+
+    private String getDataSourceUrl() {
         String databaseUrl = env.get("DATABASE_URL");
-        String userName = env.get("DATABASE_USER");
-        String password = env.get("DATABASE_PASSWORD");
-        // Si están en blanco usamos los por defecto.
         if (databaseUrl == null || databaseUrl.isBlank()) {
             databaseUrl = URL;
         }
-        if (userName == null || userName.isBlank()) {
-            userName = USERNAME;
+        return databaseUrl;
+    }
+
+    private String getDataSourceUsername() {
+        String username = env.get("DATABASE_USER");
+        if (username == null || username.isBlank()) {
+            username = USERNAME;
         }
+        return username;
+    }
+
+    private String getDatasourcePassword() {
+        String password = env.get("DATABASE_PASSWORD");
         if (password == null || password.isBlank()) {
             password = PASSWORD;
         }
-        // Configuramos el datasource.
-        dataSourceBuilder.url(databaseUrl);
-        dataSourceBuilder.username(userName);
-        dataSourceBuilder.password(password);
-        return dataSourceBuilder.build();
+        return password;
     }
 }
